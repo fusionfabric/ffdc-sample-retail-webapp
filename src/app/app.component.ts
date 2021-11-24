428866fefc97f9c62c05fa1e5ad20449837eaa9c
frigui dorsaf <dorsafrigui@gmail.com>
2021-11-24 17:42:12 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
+import { NavigationNode } from './components/app-layout/navigation-node';
+import { Store, select } from '@ngrx/store';
+import { AppState, selectRouteData, selectAllAccounts, selectRouteParams } from './store/reducers';
+import { Observable, combineLatest } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { routes } from './constants';
 
 @Component({
   selector: 'ffdc-app',
@@ -9,59 +13,49 @@ import { routes } from './constants';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  appName = 'Retail App';
+  navbarMenuData: NavigationNode[] = [];
 
-  navigationNodes = routes;
+  isHomePage$: Observable<boolean>;
 
+  constructor(store: Store<AppState>, private router: Router, private cd: ChangeDetectorRef) {
+    this.isHomePage$ = store.pipe(
+      select(selectRouteData),
+      map(data => (data ? data.viewId === 'home' : false))
+    );
 
-    constructor(private router: Router) {}
-  
-    nodeChosen(node : any) {
-      this.router.navigate([node.path]);
-    }
-  
-    brandAction() {
-      this.router.navigate(['home']);
-    }
+    combineLatest(
+      store.pipe(select(selectAllAccounts)),
+      store.pipe(select(selectRouteParams)),
+      store.pipe(select(selectRouteData))
+    ).subscribe(([accounts, routeParams, routeData]) => {
+      if (routeData) {
+        if (routeData.viewId === 'accounts') {
+          this.navbarMenuData = [
+            {
+              url: '/accounts',
+              title: 'ACCOUNTS'
+            }
+          ];
+        } else if (routeData.viewId === 'accountDetail') {
+          const currentAccount = accounts.find(account => account.accountId === routeParams.accountId);
+          this.navbarMenuData = [
+            {
+              url: '/accounts',
+              title: 'ACCOUNTS',
+              showOtherChildren: true,
+              children: accounts.map(account => ({
+                url: `/accounts/${account.accountId}`,
+                title: `${account.nickname}`
+              }))
+            },
+            {
+              url: `/accounts/${routeParams.accountId}`,
+              title: currentAccount ? currentAccount.nickname : ''
+            }
+          ];
+        }
+        this.cd.markForCheck();
+      }
+    });
   }
-  //   this.isHomePage$ = store.pipe(
-  //     select(selectRouteData),
-  //     map(data => (data ? data.viewId === 'home' : false))
-  //   );
-
-  //   combineLatest(
-  //     store.pipe(select(selectAllAccounts)),
-  //     store.pipe(select(selectRouteParams)),
-  //     store.pipe(select(selectRouteData))
-  //   ).subscribe(([accounts, routeParams, routeData]) => {
-  //     if (routeData) {
-  //       if (routeData.viewId === 'accounts') {
-  //         this.navbarMenuData = [
-  //           {
-  //             url: '/accounts',
-  //             title: 'ACCOUNTS'
-  //           }
-  //         ];
-  //       } else if (routeData.viewId === 'accountDetail') {
-  //         const currentAccount = accounts.find(account => account.accountId === routeParams.accountId);
-  //         this.navbarMenuData = [
-  //           {
-  //             url: '/accounts',
-  //             title: 'ACCOUNTS',
-  //             showOtherChildren: true,
-  //             children: accounts.map(account => ({
-  //               url: `/accounts/${account.accountId}`,
-  //               title: `${account.nickname}`
-  //             }))
-  //           },
-  //           {
-  //             url: `/accounts/${routeParams.accountId}`,
-  //             title: currentAccount ? currentAccount.nickname : ''
-  //           }
-  //         ];
-  //       }
-  //       this.cd.markForCheck();
-  //     }
-  //   });
-  // }
-  
+}
